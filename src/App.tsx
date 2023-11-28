4d3a16d5104b1ce1d2aca658cb80b45087fb90a6
Roy Rosenberg <Roy.Rosenberg@capito.co.il>
2023-11-28 19:03:41 +0200

    basic redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import { Header } from './components/Header';
+import { Main } from './Main';
+import { store } from './store/store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-7xl mx-auto px-4">
+      <Provider store={store}>
+        <Header />
+        <Main />
+      </Provider>
     </div>
   );
 }
